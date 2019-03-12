4a45586ff39df0247dd4269980f8a03e689d4376
wilson javier <javierre10va@gmail.com>
2019-03-11 23:07:39 -0500

    * get collection of entry and egress

--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -2,14 +2,23 @@ import { Injectable } from '@angular/core';
 import {IngresoEgreso} from './ingreso-egreso.model';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {AuthService} from '../auth/auth.service';
+import {Store} from '@ngrx/store';
+import {AppState} from '../app.reducer';
+import {filter, map} from 'rxjs/operators';
+import {SetItemsAction} from './ingreso-egreso.actions';
+import {Subscription} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresoEgresoService {
 
+  public ingresoEgresoListenerSubscription: Subscription = new Subscription();
+  public ingresoEgresoItemsSubscription: Subscription = new Subscription();
+
   constructor(private aFDB: AngularFirestore,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private store: Store<AppState>) { }
 
   public crearIngresoEgreso( ingresoEgreso: IngresoEgreso ) {
 
@@ -17,5 +26,45 @@ export class IngresoEgresoService {
 
     return this.aFDB.doc( `${user.uid}/ingresos-egresos`)
       .collection('items').add({...ingresoEgreso});
+  }
+
+  public initIngresoEgresoListener() {
+
+    this.ingresoEgresoListenerSubscription = this.store.select('auth')
+      .pipe(
+        filter( auth => auth.user !== null )
+      )
+      .subscribe(
+        (auth) => {
+          this.ingresoEgresoItems(auth.user.uid);
+        }
+      );
+  }
+
+  private ingresoEgresoItems( uid: string) {
+
+    this.ingresoEgresoItemsSubscription = this.aFDB.collection(`${ uid }/ingresos-egresos/items`)
+      .snapshotChanges()
+      .pipe(
+        map(
+          docData => {
+            return docData.map( doc => {
+              return{
+                uid: doc.payload.doc.id,
+                ...doc.payload.doc.data()
+              };
+            });
+          }
+        )
+      )
+      .subscribe( (coleccion: any[]) => {
+        this.store.dispatch(new SetItemsAction(coleccion));
+      });
+
+  }
+
+  private cancelSubscriptions() {
+    this.ingresoEgresoItemsSubscription.unsubscribe();
+    this.ingresoEgresoListenerSubscription.unsubscribe();
   }
 }
