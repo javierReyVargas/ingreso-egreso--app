f4b25ce40cb0687cfa835c14a7db261329b604d9
wilson javier <javierre10va@gmail.com>
2019-03-11 22:21:46 -0500

    * added actions and reducer for entry and egress

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -2,6 +2,7 @@
 
 import * as fromUi from './shared/ui.reducer';
 import * as fromAuth from './auth/auth.reducer';
+import * as fromIngresoEgreso from './ingreso-egreso/ingreso-egreso.reducer';
 
 import {ActionReducerMap} from '@ngrx/store';
 
@@ -10,10 +11,12 @@ import {ActionReducerMap} from '@ngrx/store';
 export interface AppState {
   ui: fromUi.State;
   auth: fromAuth.AuthState;
+  ingresoEgreso: fromIngresoEgreso.IngresoEgresoState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
   ui: fromUi.uiReducer,
-  auth: fromAuth.authReducer
+  auth: fromAuth.authReducer,
+  ingresoEgreso: fromIngresoEgreso.ingresoEgresoReducer
 };
 
